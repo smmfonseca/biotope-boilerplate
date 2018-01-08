3b8f2dc8a2956961af05009c9405d4e2fd878807
Jan Rembold <jan.rembold@virtual-identity.com>
2018-01-08 12:13:15 +0100

    added more demo tests

--- test/backstop/html_report/config.js
+++ test/backstop/html_report/config.js
@@ -4,11 +4,10 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/backstop_default_Icons_0_document_0_phone.png",
-        "test": "../bitmaps_test/20180108-112955/backstop_default_Icons_0_document_0_phone.png",
+        "test": "../bitmaps_test/20180108-121214/backstop_default_Icons_0_document_0_phone.png",
         "selector": "document",
         "fileName": "backstop_default_Icons_0_document_0_phone.png",
         "label": "Icons",
-        "requireSameDimensions": true,
         "misMatchThreshold": 0.1,
         "diff": {
           "isSameDimensions": true,
@@ -16,21 +15,18 @@ report({
             "width": 0,
             "height": 0
           },
-          "misMatchPercentage": "1.55",
-          "analysisTime": 34
-        },
-        "diffImage": "../bitmaps_test/20180108-112955/failed_diff_backstop_default_Icons_0_document_0_phone.png"
+          "misMatchPercentage": "0.00"
+        }
       },
-      "status": "fail"
+      "status": "pass"
     },
     {
       "pair": {
         "reference": "../bitmaps_reference/backstop_default_Icons_0_document_1_tablet.png",
-        "test": "../bitmaps_test/20180108-112955/backstop_default_Icons_0_document_1_tablet.png",
+        "test": "../bitmaps_test/20180108-121214/backstop_default_Icons_0_document_1_tablet.png",
         "selector": "document",
         "fileName": "backstop_default_Icons_0_document_1_tablet.png",
         "label": "Icons",
-        "requireSameDimensions": true,
         "misMatchThreshold": 0.1,
         "diff": {
           "isSameDimensions": true,
@@ -38,12 +34,48 @@ report({
             "width": 0,
             "height": 0
           },
-          "misMatchPercentage": "0.30",
-          "analysisTime": 56
-        },
-        "diffImage": "../bitmaps_test/20180108-112955/failed_diff_backstop_default_Icons_0_document_1_tablet.png"
+          "misMatchPercentage": "0.00"
+        }
       },
-      "status": "fail"
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Grid_33_33_33_0_colcol--large-4_0_phone.png",
+        "test": "../bitmaps_test/20180108-121214/backstop_default_Grid_33_33_33_0_colcol--large-4_0_phone.png",
+        "selector": ".col.col--large-4",
+        "fileName": "backstop_default_Grid_33_33_33_0_colcol--large-4_0_phone.png",
+        "label": "Grid 33/33/33",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Grid_33_33_33_0_colcol--large-4_1_tablet.png",
+        "test": "../bitmaps_test/20180108-121214/backstop_default_Grid_33_33_33_0_colcol--large-4_1_tablet.png",
+        "selector": ".col.col--large-4",
+        "fileName": "backstop_default_Grid_33_33_33_0_colcol--large-4_1_tablet.png",
+        "label": "Grid 33/33/33",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
     }
   ]
 });
