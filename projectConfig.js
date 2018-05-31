b80daa45bcf3cf2fdc230b24b0cb99850cecf232
SheepFromHeaven <marcwissler@gmail.com>
2018-05-31 11:41:32 +0200

    Add polyfills to work for firefox

--- projectConfig.js
+++ projectConfig.js
@@ -7,10 +7,20 @@ module.exports = {
 			'jquery': 'dist/jquery.js',
 			'jquery-migrate': 'dist/jquery-migrate.js',
 			'normalize.css': 'normalize.css',
-			'@webcomponents/webcomponentsjs': ['webcomponents-loader.js', 'custom-elements-es5-adapter.js']
+			'@webcomponents/webcomponentsjs': [
+				'webcomponents-loader.js',
+				'custom-elements-es5-adapter.js',
+				'webcomponents-hi.js',
+				'webcomponents-hi.js.map',
+				'webcomponents-hi-ce.js',
+				'webcomponents-hi-ce.js.map',
+				'webcomponents-hi-sd.js',
+				'webcomponents-hi-sd.js.map',
+				'webcomponents-hi-sd-ce.js',
+				'webcomponents-hi-sd-ce.js.map'
+			]
 		}
 	},
-
 	uglify: {
 		ignoreList: [
 			'resources/js/vendor/custom-elements-es5-adapter.js'
